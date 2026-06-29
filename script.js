62d291c76fb3a828fa614de602788a15b43c8bc1
ahbuuuuu <4a670040us@gmail.com>
2026-06-30 03:57:55 +0800

    script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@
 // ────────────────────────────────────────────
 // Supabase 設定
 // ────────────────────────────────────────────
-const SUPABASE_URL = "https://jnpddmlnikjtvqrgbtjo.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_i9TmHKHUZdlSVnFgP21eTQ_dyYIKLml";
+const SUPABASE_URL = "https://dzneiplvifzrlyvnuqya.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6bmVpcGx2aWZ6cmx5dm51cXlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3NDk4NDIsImV4cCI6MjA5ODMyNTg0Mn0.5c4rGg91CCVIY-We9fjuJlYQlJ_lRFRWcq-jlY1cN8U";
 
 const db = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
     auth: { persistSession: false }
@@ -309,4 +309,3 @@ window.onload = async () => {
         bar.textContent = i18n[getLang()].wait;
     }
 };
-
